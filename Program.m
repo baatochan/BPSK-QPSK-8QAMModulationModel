28324ee164ce2bad6940d6b5b784771fc9c1b7dc
Roudie <sebak19962@gmail.com>
2017-05-09 13:48:43 +0200

    Program.m wyswietla scatter w roznych kolorach

--- Program.m
+++ Program.m
@@ -1,43 +1,49 @@
-%%MAIN PROGRAM CLASS
-
 clear;
 
 %zmienne manipuluj?ce program
 BIT_NUMBER = 1000; %ilosc bitow do wygenerowania
 WAWE_FREQUENCY = 1000; %[Hz] czestotliwosc fali nosnej
-sigmaU=0.02;                            %odchylenie standardowe U
-sigmaOmega=0.005;                        %odchylenie standardowe Omegi
+sigmaU=0.1;                            %odchylenie standardowe U
+sigmaOmega=0.1;                        %odchylenie standardowe Omegi
 
-bitArray = RandomBitsGenerator(BIT_NUMBER);
+bitArray = RandomBitsGenerator(BIT_NUMBER)
 
-[carrierWawe, timeAxis] = QPSKModulator(WAWE_FREQUENCY, BIT_NUMBER, bitArray);
-[carrierWawe2, timeAxis2] = BPSKModulator(WAWE_FREQUENCY, BIT_NUMBER, bitArray);
 [carrierWawe3, timeAxis3, numberOfSamplesInSymbol3, x3, y3] = ChannelQPSK(WAWE_FREQUENCY, BIT_NUMBER, bitArray, sigmaU, sigmaOmega);
 [carrierWawe4, timeAxis4, numberOfSamplesInSymbol4, x4, y4] = ChannelPSK(WAWE_FREQUENCY, BIT_NUMBER, bitArray, sigmaU, sigmaOmega);
 
-figure(1)
-subplot(2,1,1);
-plot (timeAxis,carrierWawe,'b')
-xlabel ('czas[s]');
-ylabel ('sygnal');
-subplot(2,1,2);
-plot (timeAxis2,carrierWawe2,'b')
-xlabel ('czas[s]');
-ylabel ('sygnal');
-%
-figure(2)
-subplot(2,1,1);
-plot (timeAxis3,carrierWawe3,'b')
-xlabel ('czas[s]');
-ylabel ('sygnal');
-subplot(2,1,2);
-plot (timeAxis4,carrierWawe4,'b')
-xlabel ('czas[s]');
-ylabel ('sygnal');
+
+for i=1:BIT_NUMBER
+    if bitArray(i) == 0
+        colorPSK(i)=1;
+    end
+    if bitArray(i) == 1
+        colorPSK(i)=2;
+    end
+end
+
+for i = 1:(BIT_NUMBER/2)
+        if bitArray(2*i) == 0
+                if bitArray((2*i)-1) == 0
+                    colorQPSK(i) = 1;
+                end 
+                if bitArray((2*i)-1) == 1
+                    colorQPSK(i) = 2;
+                end
+        end
+        if bitArray(2*i) == 1
+                if bitArray((2*i)-1) == 0
+                    colorQPSK(i) = 3;
+                end 
+                if bitArray((2*i)-1) == 1
+                    colorQPSK(i) = 4;
+                end
+        end
+end
+
 
 figure(3)
-scatter(x3,y3)
+scatter(x3,y3,[],colorQPSK,'filled');
 axis([-1 1 -1 1]);
 figure(4)
-scatter(x4,y4)
+scatter(x4,y4,[],colorPSK,'filled');
 axis([-1 1 -1 1]);
